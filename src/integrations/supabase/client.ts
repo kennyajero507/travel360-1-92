81d959b8029ba7597e452a0fe9362a8c66a1226c
kennyajero507 <150891997+kennyajero507@users.noreply.github.com>
2025-06-15 10:38:02 +0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://mislxibukncnmvwhsiml.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pc2x4aWJ1a25jbm12d2hzaW1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1ODQ0NTYsImV4cCI6MjA2MzE2MDQ1Nn0.K7M9I0zJfbYhXZtHYqJVr3G8AzuLl1xOBpF0zjxZxro"
+const supabaseUrl = "https://gtwgyakeufqghnnmeovh.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0d2d5YWtldWZxZ2hubm1lb3ZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5NzAwNjgsImV4cCI6MjA2NTU0NjA2OH0.ygMAMIEPsUA-Kx0BXXWxhQTXGL2zLTYD1dP3AVIE2gI"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
